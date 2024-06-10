5c243528d31e07f9f4fcca42b4c03b53eb96414c
Aditya <sharmaaditya13064@gmail.com>
2024-06-10 23:44:58 +0530

    Home page design

--- portfolio-web/src/app/home/home-component.jsx
+++ portfolio-web/src/app/home/home-component.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import Stack from '@mui/material/Stack';
+import MyImage from '../../assets/myimg.png'
+import { Typography } from '@mui/material';
+import useStyles from './home-styles';
 
 const HomeComponent = () =>{
+    const classes = useStyles();
+
     return(
-        <Stack height={'100vh'} width={'100vw'} sx={{}}>
+        <Stack height={'100vh'} width={'100vw'} sx={{
+            backgroundImage:`url(${MyImage})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize:'cover',
+            alignItems:'center',
+            justifyContent:'center',
+        }}>
               <div id='home'></div>
+              <Stack flexDirection={'column'} alignSelf={'flex-start'} marginLeft={'25em'}>
+                <Typography variant='h2' color={'#fff'}>
+                    {'Hello'}
+                </Typography>
+                <Typography variant='h1' color={'#fff'}>{'I\'m Aditya'}</Typography>
+                <Typography variant='h4' color={'#fff'}>{'Web Developer'}</Typography>
+              </Stack>
         </Stack>
 )};
 
--- portfolio-web/src/app/navbar/navbar-component.jsx
+++ portfolio-web/src/app/navbar/navbar-component.jsx
@@ -20,19 +20,6 @@ const NavbarComponent = () =>{
             }, 500);
         };
         window.addEventListener('scroll', handleScroll);
-        const linkElements = document.querySelectorAll('item');
-
-    const animateit = (e) => {
-      const { offsetX: x, offsetY: y } = e;
-      const { offsetWidth: width, offsetHeight: height } = e.target;
-
-      const move = 25;
-      const xMove = x / width * (move * 2) - move;
-      const yMove = y / height * (move * 2) - move;
-
-      // Apply the transform to the cursor
-      cursorRef.current.style.transform = `translate3d(${xMove}px, ${yMove}px, 0)`;
-    };
 
     const editCursor = (e) => {
         const { clientX: x, clientY: y } = e;
@@ -42,32 +29,22 @@ const NavbarComponent = () =>{
         cursorRef.current.style.top = `${y - cursorHeight}px`;
       };
 
-    linkElements.forEach((link) => {
-      link.addEventListener('mousemove', animateit);
-      link.addEventListener('mouseleave', animateit);
-    });
-
     window.addEventListener('mousemove', editCursor);
         return () => {
             window.removeEventListener('scroll', handleScroll);
-            linkElements.forEach((link) => {
-                link.removeEventListener('mousemove', animateit);
-                link.removeEventListener('mouseleave', animateit);
-              });
-        
               window.removeEventListener('mousemove', editCursor);
         };
     }, []);
     return(
         <Stack  className={classes.wrapper} top={isVisible ? '0' : '-100px'}> 
-            <Stack className={classes.itemWrapper} flexDirection={'row'} justifyContent={'center'} gap={8}>
+            <Stack className={classes.itemWrapper} flexDirection={'row'} justifyContent={'center'}>
                 <span><Link activeClass='active' to="home" spy={true} smooth={true} duration={500} className={classes.item}>Home</Link></span>
                 <span><Link activeClass='active' to="about" spy={true} smooth={true} duration={500} className={classes.item}>About</Link></span>
                 <span><Link activeClass='active' to="skills" spy={true} smooth={true} duration={500} className={classes.item}>Skills</Link></span>
                 <span><Link activeClass='active' to="articles" spy={true} smooth={true} duration={500} className={classes.item}>Articles</Link></span>
                 <span><Link activeClass='active' to="projects" spy={true} smooth={true} duration={500} className={classes.item}>Projects</Link></span>
+                <div className={classes.cursor} ref={cursorRef}></div>
             </Stack>       
-            <div className={classes.cursor} ref={cursorRef}></div>
         </Stack>
 )};
 
