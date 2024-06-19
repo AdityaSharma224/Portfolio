6ee899b4ad7e3726c4959b4dda7ac689b1102947
ADITYA SHARMA <84766632+AdityaSharma224@users.noreply.github.com>
2024-06-19 14:33:11 +0530

    unused spaces

--- src/app/article/article-component.jsx
+++ src/app/article/article-component.jsx
@@ -3,14 +3,10 @@ import Stack from '@mui/material/Stack';
 
 const ArticleComponent = () =>{
     return(
-        
-
         <Stack height={'100vh'} width={'100vw'} sx={{backgroundColor:'green'}}>
             article
             <div id='articles'></div>
         </Stack>
-       
-       
 )};
 
 export default ArticleComponent;
