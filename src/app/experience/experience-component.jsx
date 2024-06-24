42093f4165f0a4a464659f63f06affe30a01690d
Aditya <sharmaaditya13064@gmail.com>
2024-06-24 23:00:37 +0530

    articles section

--- src/app/experience/experience-component.jsx
+++ src/app/experience/experience-component.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Stack from "@mui/material/Stack";
 import useStyles from "./experience-styles";
 import MyAboutImage from "../../assets/about-image.jpg";
-import { Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import GFGIcon from "../../assets/gfg.png";
 import CASTIcon from "../../assets/cast.png";
 import CONTLOIcon from "../../assets/contlo.png";
@@ -13,56 +13,44 @@ import {
 } from "../constants";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import { CardActionArea } from "@mui/material";
-import Button from "@mui/material/Button";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
 
 const ExperienceComponent = () => {
   const classes = useStyles();
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const experiences = [
     {
-      icon: (
-        <Stack
-          component={"img"}
-          borderRadius={"20px"}
-          src={CASTIcon}
-          className={classes.cLogo}
-        />
-      ),
-      year: "10/2023  -  Present",
+      icon: CASTIcon,
+      year: "10/2023 - Present",
       title: "Software Development Engineer - 1",
       description: CAST_DESCRIPTION,
     },
     {
-      icon: (
-        <Stack
-          component={"img"}
-          borderRadius={"20px"}
-          src={CONTLOIcon}
-          height={"50px"}
-          width={"120px"}
-        />
-      ),
-      year: "07/2023  -  10/2023",
+      icon: CONTLOIcon,
+      year: "07/2023 - 10/2023",
       title: "Software Development Engineer Intern",
       description: CONTLO_DESCRIPTION,
     },
     {
-      icon: (
-        <Stack
-          component={"img"}
-          borderRadius={"20px"}
-          src={GFGIcon}
-          height={"50px"}
-          width={"200px"}
-        />
-      ),
-      year: "10/2022  -  06/2023",
+      icon: GFGIcon,
+      year: "10/2022 - 06/2023",
       title: "Member of Technical Staff Intern",
       description: GFG_DESCRIPTION,
     },
   ];
+
+  const goToPrevCard = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + experiences.length) % experiences.length
+    );
+  };
+
+  const goToNextCard = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % experiences.length);
+  };
 
   return (
     <Stack className={classes.wrapper} id="experience">
@@ -73,7 +61,7 @@ const ExperienceComponent = () => {
           width={"40%"}
           height={"100%"}
           alt="myimage"
-        ></img>
+        />
         <Stack className={classes.aboutWrapper}>
           <Stack className={classes.experienceWrapper}>
             <Typography
@@ -91,32 +79,66 @@ const ExperienceComponent = () => {
                 fontWeight: { xs: 700, sm: 700, md: 700, lg: 700, xl: 700 },
               }}
             >
-              {"Experience"}
+              Experience
             </Typography>
             <Stack
               sx={{ height: "100%", width: "100%", alignItems: "center" }}
               flexDirection={"row"}
               gap={2}
-              borderRadius={'20px'}
+              borderRadius={"20px"}
             >
-              <Button>{"Prev"}</Button>
-              <Card sx={{ maxWidth: 600, minWidth:600,height: 600, borderRadius:'20px' }}>
-                <CardActionArea sx={{borderRadius:'20px'}}>
-                   <Stack padding={2} flexDirection={'row'} alignItems={'flex-end'} justifyContent={'space-between'} sx={{borderRadius:'20px'}}>
-                    <img src={CASTIcon} height={'80px'} width={'280px'}></img>
-                    <Typography variant="h6">{'tenure'}</Typography>
-                   </Stack>
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {'Software Development Engineer'}
-                    </Typography>
-                    <Typography variant="body2" color="text">
-                      {''}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-              <Button>{"Next"}</Button>
+              <IconButton onClick={goToPrevCard}>
+                <KeyboardDoubleArrowLeftIcon
+                  htmlColor="#fff"
+                  fontSize="large"
+                />
+              </IconButton>
+              {experiences.map(
+                (experience, index) =>
+                  index === currentIndex && (
+                    <Card
+                      key={index}
+                      sx={{
+                        maxWidth: 600,
+                        minWidth: 600,
+                        height: 600,
+                        borderRadius: "20px",
+                      }}
+                    >
+                      <CardActionArea sx={{ borderRadius: "20px" }}>
+                        <Stack
+                          padding={2}
+                          flexDirection={"row"}
+                          alignItems={"flex-end"}
+                          justifyContent={"space-between"}
+                          sx={{ borderRadius: "20px" }}
+                        >
+                          <img
+                            src={experience.icon}
+                            height={"80px"}
+                            width={"280px"}
+                            alt={experience.title}
+                          />
+                          <Typography variant="h6">{experience.year}</Typography>
+                        </Stack>
+                        <CardContent>
+                          <Typography gutterBottom variant="h5" component="div">
+                            {experience.title}
+                          </Typography>
+                          <Typography variant="body2" color="text">
+                            {experience.description}
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
+                    </Card>
+                  )
+              )}
+              <IconButton onClick={goToNextCard}>
+                <KeyboardDoubleArrowRightIcon
+                  htmlColor="#fff"
+                  fontSize="large"
+                />
+              </IconButton>
             </Stack>
           </Stack>
         </Stack>
