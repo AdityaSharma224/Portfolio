5e08aacccb6a198bf247655af37fdb304896b79c
ADITYA SHARMA <84766632+AdityaSharma224@users.noreply.github.com>
2024-06-25 11:28:13 +0530

    updating experience section

--- src/app/experience/experience-component.jsx
+++ src/app/experience/experience-component.jsx
@@ -13,6 +13,8 @@ import {
 } from "../constants";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 import { CardActionArea } from "@mui/material";
 import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
@@ -26,19 +28,36 @@ const ExperienceComponent = () => {
       icon: CASTIcon,
       year: "10/2023 - Present",
       title: "Software Development Engineer - 1",
-      description: CAST_DESCRIPTION,
+      description: <List sx={{ listStyleType: 'disc', pl: 2, '& .MuiListItem-root': { display: 'list-item' } }}>
+      <ListItem>{'Developed a delicate desktop application to facilitate comprehensive source code analysis, understanding and architectural insights.'}</ListItem>
+      <ListItem>{'Frontend development of the CAST Imaging Analyzer, Utilizing the Wails framework with React.js for an intuitive user interface,catering to diverse operational environments.'}</ListItem>
+      <ListItem>{'Developed a dual-mode functionality-'}</ListItem>
+      <ListItem>{'Online Mode: Implemented a lightweight CAST analyzer that performs rapid scans of applications, identifies underlying technologies,
+and seamlessly integrates with CAST Imaging SaaS for automatic result uploads, enhancing efficiency and security.'}</ListItem>
+      <ListItem>{'Offline Mode: Established a robust local setup designed for comprehensive scans without internet dependency, optimizing for storage
+efficiency. Facilitated post-scan reviews, enabling precise manual uploads to CAST Imaging SaaS, ensuring data integrity and control.'}</ListItem>
+        <ListItem>{'Engineered an intuitive interface for efficient navigation, and initiation of the scanning process, ensuring a user-friendly experience'}</ListItem>
+    </List>,
     },
     {
       icon: CONTLOIcon,
       year: "07/2023 - 10/2023",
       title: "Software Development Engineer Intern",
-      description: CONTLO_DESCRIPTION,
+     description: <List sx={{ listStyleType: 'disc', pl: 2, '& .MuiListItem-root': { display: 'list-item' } }}>
+      <ListItem>{'Development of long-term memory capabilities for large language models, enhancing response relevance and accuracy by 65%.'}</ListItem>
+      <ListItem>{'Databases Integration (Redis, Pinecone) for vector storage.'}</ListItem>
+      <ListItem>{'Implemented innovative AI algorithms that improved overall system efficiency, resulting in a 30% reduction in response.'}</ListItem>
+    </List>,
     },
     {
       icon: GFGIcon,
       year: "10/2022 - 06/2023",
       title: "Member of Technical Staff Intern",
-      description: GFG_DESCRIPTION,
+      description: <List sx={{ listStyleType: 'disc', pl: 2, '& .MuiListItem-root': { display: 'list-item' } }}>
+      <ListItem>{'Worked closely with the Core Data Structure and Algorithm team at GeeksforGeeks.'}</ListItem>
+      <ListItem>{'Designed and developed the most viewed data structure and algorithm official tutorials e.g., Dijkstra's Algorithm, Set, Map, MaxHeap, and Min-Heap.'}</ListItem>
+      <ListItem>{'Improved Customer/Viewer Experience by delivering top quality content and Visuals.'}</ListItem>
+    </List>,
     },
   ];
 
